55847d775f64cfbc4eb190d06e8118c9a81712e2
Justin A <jarata@gmail.com>
2021-03-14 11:46:28 -0700

    Second Function created second arrow function and compiled

---
+++ typescript.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    return a + b;
+}
+var sum2 = function (a, b) {
+    return a + b;
+};
--- typescript.ts
+++ typescript.ts
@@ -1,3 +1,7 @@
 function sum(a, b) {
     return a + b;
 }
+
+const sum2 = (a, b) => {
+    return a + b;
+}
