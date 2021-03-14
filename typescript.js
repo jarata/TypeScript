4bc70acae4c1906013e1ab2f68f0667ecefad027
Justin A <jarata@gmail.com>
2021-03-14 11:51:13 -0700

    Third Function created third function with types and compiled

--- typescript.js
+++ typescript.js
@@ -4,3 +4,6 @@ function sum(a, b) {
 var sum2 = function (a, b) {
     return a + b;
 };
+var sum3 = function (a, b) {
+    return a + b;
+};
--- typescript.ts
+++ typescript.ts
@@ -5,3 +5,7 @@ function sum(a, b) {
 const sum2 = (a, b) => {
     return a + b;
 }
+
+const sum3 = (a: number, b: number) => {
+    return a + b;
+}
