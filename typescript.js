6f580376da23d6a15feb1662de1ca2f63a4bc609
Justin A <jarata@gmail.com>
2021-03-14 11:56:59 -0700

    Answers added answers and console logs then compiled

--- typescript.js
+++ typescript.js
@@ -1,9 +1,15 @@
 function sum(a, b) {
     return a + b;
 }
+var answer = sum(4, 5);
+console.log(answer);
 var sum2 = function (a, b) {
     return a + b;
 };
+var answer2 = sum2(5, 6);
+console.log(answer2);
 var sum3 = function (a, b) {
     return a + b;
 };
+var answer3 = sum3(6, 7);
+console.log(answer3);
--- typescript.ts
+++ typescript.ts
@@ -2,10 +2,18 @@ function sum(a, b) {
     return a + b;
 }
 
+const answer = sum(4, 5);
+console.log(answer);
+
 const sum2 = (a, b) => {
     return a + b;
 }
+const answer2 = sum2(5, 6);
+console.log(answer2);
 
 const sum3 = (a: number, b: number) => {
     return a + b;
 }
+
+const answer3 = sum3(6, 7);
+console.log(answer3);
