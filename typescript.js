5ef7cceaf991839d208865c3b9f57173244a1c86
Justin A <jarata@gmail.com>
2021-03-15 12:25:40 -0700

    Types created types and ran watch compiler

--- typescript.js
+++ typescript.js
@@ -13,3 +13,19 @@ var sum3 = function (a, b) {
 };
 var answer3 = sum3(6, 7);
 console.log(answer3);
+// boolean
+var isCool = true;
+// number
+var age = 56;
+// string
+var eyeColor = "brown";
+var favoriteQuote = "I am " + age + " years old";
+// arrays
+var pets = ["pig", "dog", "cat"];
+var birds = ["seagull", "sparrow", "pidgeon"];
+var wizard = {
+    a: 'John'
+};
+// null or undefined
+var meh = undefined;
+var no = null;
--- typescript.ts
+++ typescript.ts
@@ -17,3 +17,25 @@ const sum3 = (a: number, b: number) => {
 
 const answer3 = sum3(6, 7);
 console.log(answer3);
+
+// boolean
+let isCool: boolean = true;
+
+// number
+let age: number = 56;
+
+// string
+let eyeColor: string = "brown";
+let favoriteQuote: string = `I am ${age} years old`;
+
+// arrays
+let pets: string[] = ["pig", "dog", "cat"];
+let birds: Array<string> = ["seagull", "sparrow", "pidgeon"];
+
+let wizard: object = {
+    a: 'John'
+}
+
+// null or undefined
+let meh: undefined = undefined
+let no: null = null;
